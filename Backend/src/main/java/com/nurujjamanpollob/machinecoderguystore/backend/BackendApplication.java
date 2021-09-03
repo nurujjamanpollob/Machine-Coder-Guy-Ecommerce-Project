030f25948aa6bf830e76e689b3dec230345beb62
nurujjamanpollob <nurujjamanpollob@gmail.com>
2021-09-03 09:28:48 +0600

    typo fixes

--- Backend/src/main/java/com/nurujjamanpollob/machinecoderguystore/backend/BackendApplication.java
+++ Backend/src/main/java/com/nurujjamanpollob/machinecoderguystore/backend/BackendApplication.java
@@ -1,12 +1,13 @@
 package com.nurujjamanpollob.machinecoderguystore.backend;
 
-import com.nurujjamanpollob.machinecoderguystore.commonlibrary.Role;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EntityScan(basePackageClasses = {Role.class})
+@EntityScan({"com.nurujjamanpollob.machinecoderguystore.commonlibrary", "com.nurujjamanpollob.machinecoderguystore.backend.users"})
+@ComponentScan(basePackages = { "com.nurujjamanpollob.machinecoderguystore.*" })
 public class BackendApplication {
 
     public static void main(String[] args) {
--- Backend/src/test/java/com/nurujjamanpollob/machinecoderguy/backend/test/RoleRepositoryTests.java
+++ Backend/src/test/java/com/nurujjamanpollob/machinecoderguy/backend/test/RoleRepositoryTests.java
@@ -4,9 +4,12 @@ import com.nurujjamanpollob.machinecoderguystore.backend.users.RoleRepository;
 import com.nurujjamanpollob.machinecoderguystore.commonlibrary.Role;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -20,6 +23,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @ContextConfiguration(classes = {RoleRepository.class})
 @EnableJpaRepositories(basePackages = {"com.nurujjamanpollob.machinecoderguystore.backend.*"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@EntityScan({"com.nurujjamanpollob.machinecoderguystore.commonlibrary", "com.nurujjamanpollob.machinecoderguystore.backend.users"})
+@ComponentScan(basePackages = { "com.nurujjamanpollob.machinecoderguystore.*" })
+@Rollback(value = false)
 public class RoleRepositoryTests {
 
     @Autowired
@@ -27,7 +33,7 @@ public class RoleRepositoryTests {
 
     // create first test! yeeehoo!!!
     @Test
-    public void createFirstTest(){
+    public void createAdminTest(){
 
         // create test for admin role
         Role adminRole = new Role("Admin", "The admin is can manage everything");
@@ -40,4 +46,80 @@ public class RoleRepositoryTests {
 
 
     }
+
+
+    @Test
+    public void createSalesPersonRoleTest(){
+
+        // create test for sales person role
+        Role salesPersonRole = new Role("Salesperson", "Manage product price" +
+                ", customers, shipping, order & sales report");
+
+
+
+
+
+
+        // save newly created data to database and assign to new object
+        Role role =repository.save(salesPersonRole);
+
+        //try to check if data is not null!
+        assertThat(role.getRoleName()).isNotNull();
+
+
+    }
+
+
+
+    @Test
+    public void createEditorRoleTest(){
+
+
+        // create a new role for editor
+        Role editorRole = new Role("Editor", "Manage categories" +
+                ", brands, products, articles & menus");
+
+        // save newly created data to database and assign to new object
+        Role role =repository.save(editorRole);
+
+        //try to check if data is not null!
+        assertThat(role.getRoleName()).isNotNull();
+
+    }
+
+
+
+    @Test
+    public void createShipperRoleTest(){
+
+
+        // create a new role for shipper
+        Role shipperRole = new Role("Shipper", "View products" +
+                ", View Order, update order Status");
+
+        // save newly created data to database and assign to new object
+        Role role =repository.save(shipperRole);
+
+        //try to check if data is not null!
+        assertThat(role.getRoleName()).isNotNull();
+
+    }
+
+    @Test
+    public void createAssistantRoleTest(){
+
+
+        // create a new role for shipper
+        Role assistantRole = new Role("Assistant", "Manage questions" +
+                "and reviews of product");
+
+        // save newly created data to database and assign to new object
+        Role role =repository.save(assistantRole);
+
+        //try to check if data is not null!
+        assertThat(role.getRoleName()).isNotNull();
+
+    }
+
+
 }
