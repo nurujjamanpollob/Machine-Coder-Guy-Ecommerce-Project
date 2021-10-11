a3a28dc598e9a106072a4e183df166add1fd3863
nurujjamanpollob <nurujjamanpollob@gmail.com>
2021-10-11 09:52:34 +0600

    Create MachineCoderGuySecurity.java to allow user use our application without any authentication

--- Backend/src/main/java/com/nurujjamanpollob/machinecoderguystore/backend/security/MachineCoderGuySecurity.java
+++ Backend/src/main/java/com/nurujjamanpollob/machinecoderguystore/backend/security/MachineCoderGuySecurity.java
@@ -16,7 +16,30 @@
 
 package com.nurujjamanpollob.machinecoderguystore.backend.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class MachineCoderGuySecurity extends WebSecurityConfigurerAdapter {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // allow all request without authorization
+        http.authorizeRequests().anyRequest().permitAll();
+
+    }
 }
