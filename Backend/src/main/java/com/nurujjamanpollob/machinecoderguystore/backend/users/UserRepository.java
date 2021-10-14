2e61dcfcbfdf06c163758001c5017b5feb69e67c
nurujjamanpollob <nurujjamanpollob@gmail.com>
2021-10-14 16:09:32 +0600

    create sql query to check user email is unique or not

--- Backend/src/main/java/com/nurujjamanpollob/machinecoderguystore/backend/users/UserRepository.java
+++ Backend/src/main/java/com/nurujjamanpollob/machinecoderguystore/backend/users/UserRepository.java
@@ -17,10 +17,18 @@
 package com.nurujjamanpollob.machinecoderguystore.backend.users;
 
 import com.nurujjamanpollob.machinecoderguystore.commonlibrary.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 // create interface UserRepository  CrudRepository<User, ID>
+@SuppressWarnings({"UnusedDeclaration"})
 public interface UserRepository extends CrudRepository<User, Long> {
+
+
+    // query user by email, to check if user email is unique or not
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User getUserByEmail(@Param("email") String email);
 
 
 }
